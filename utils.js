97cf76a5b7c9fe127fae36de12da4495daaba2ef
StormCnn <118617287+StormCnn@users.noreply.github.com>
2026-03-05 16:26:59 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:26:45.720Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T13:27:00.303Z
+Commit: Fix CSS styling bug on mobile devices
