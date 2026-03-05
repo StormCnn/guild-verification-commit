be97ad6f7770794111a7aee1d13a1a6619b977c7
StormCnn <118617287+StormCnn@users.noreply.github.com>
2026-03-05 16:24:52 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:24:46.207Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:24:53.678Z
+Commit: Add dark mode support
