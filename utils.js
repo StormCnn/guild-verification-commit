471f79d0efd59b95fae413045682c4d777e2e3d4
StormCnn <118617287+StormCnn@users.noreply.github.com>
2026-03-05 16:25:46 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:25:35.248Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:25:47.167Z
+Commit: Resolve null pointer exception in user service
