5f7b5a18e273e6aeb8927b967f454a917c7a9e29
StormCnn <118617287+StormCnn@users.noreply.github.com>
2026-03-05 16:25:34 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:24:54.903Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:25:35.248Z
+Commit: Fix memory leak in event handlers
