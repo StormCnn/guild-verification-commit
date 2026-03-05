e8387be7800c5337b40f6d9599ab057a64bfd332
StormCnn <118617287+StormCnn@users.noreply.github.com>
2026-03-05 16:25:16 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:25:11.839Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T13:25:17.296Z
+Commit: Resolve timezone issues in date handling
